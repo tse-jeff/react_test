4678adcaac92b2f59319edc21cecf73018d5e6b1
tse-jeff <jtse0316@gmail.com>
2021-03-02 01:59:43 -0500

    react component

--- simple_test.jsx
+++ simple_test.jsx
@@ -8,8 +8,47 @@ const intro = (
             This is just a simple test with the basics of React 
             and JSX. Need to figure out how to render/test/deploy this?
         </p>
+        <br />
+        <p>This is just a JSX component?</p>
     </div>
 )
 
+class ComponentStuff extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                <p>
+                    Learned how to create React components and how they
+                    nested within another component.
+                </p>
+                <br />
+                <p>Everything needs to return only one element.</p>
+            </div>
+        )
+    }
+}
+
+class Component extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>A React Component</h2>
+                <ComponentStuff />
+            </div>
+        )
+    }
+}
+
 // should render the intro element if there is the root id
 ReactDOM.render(intro, document.getElementById('root'));
+
+// New component to add to the ReactDOM
+ReactDOM.render(<Component />, document.getElementById('component'));
